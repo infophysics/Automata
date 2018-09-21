d28875fcf8a68aac0f2debfc2c6a571c8c24e62a
infophysics <ncarrara@albany.edu>
2018-09-20 20:58:41 -0400

    added new tests to test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from automata.automata import Automata, Cell
+from cubicalRipser.cubicalRipser import CubicalRipser2D
 import numpy as np
 import matplotlib.pyplot as plt
 from diphaFormat import convert_csv_to_dipha
@@ -15,6 +16,10 @@ test.saveSequenceToFile(sequence, "test2.csv")
 
 #   save as binary file for DIPHA
 convert_csv_to_dipha("test2.csv","test2.complex")
+
+#   run cubical ripser
+ripser = CubicalRipser2D()
+ripser.ComputeBarcode("test2.complex", "output.csv", "DIPHA", "COMPUTEPAIRS", 99999, True)
 
 plt.figure(figsize = (10,10))
 plt.imshow(sequence, interpolation='nearest')
